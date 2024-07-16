26b74c9033ba33c675cc224f65ae3e83248c5163
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-07-16 23:43:45 +0600

    NextJs-Practice

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,13 +13,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}
         {
-          pathname !== "userList" ?
+          pathname !== "/userList" ?
           <ul className="Navbar">
           <li><Link href="/" className={'${pathname === "/" ? "active" : ""}'}>Home</Link></li>
           <li><Link href = "/about" className={'${pathname === "/about" ? "active" : ""}'}>About</Link></li>
           <li><Link href="/userList">userList</Link></li>
           <li><Link href="/registration">Contact</Link></li>
-        </ul> : null
+        </ul> : <Link href='/'>Go Home</Link>
         }
       </body>
     </html>
